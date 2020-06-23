1ebf598cceba274ce05ccf4efb69eba18570e1d6
Joseph Tawil <jtawil2015@gmail.com>
2020-06-23 05:23:09 -0700

    added .env file to hide private keys

--- connection.js
+++ connection.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql");
+require("dotenv").config();
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "yuseffufu1997",
-    database: "todo_db",
+    host: process.env.host,
+    port: process.env.port,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
 });
 
 connection.connect((err) => {
